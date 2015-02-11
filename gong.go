b50a40de394a06cb0df60f41e17aadf999e57f57
Timo Virkkunen <timo@comsec.ninja>
2015-02-11 18:48:33 +0200

    Update gong.go
    Add an option to set root directory with a command line argument

--- gong.go
+++ gong.go
@@ -25,21 +25,21 @@ func logger(h http.Handler) http.Handler {
 func main() {
 	// Different port can be assigned with command line variable -port
 	flag.StringVar(&port, "port", "8080", "Assign a port for the server to listen to. Defaults to 8080.")
+	flag.StringVar(&cwd, "root", "", "Set a root directory for server. Defaults to current working directory.")
 	flag.Parse()
 
-	// Print the assigned port before prepending ":"
+	// Print the assigned port
 	log.Println("Listening on port", port)
 
-	// http.ListenAndServer requires port in a string form, prefixed with ":"
-	port = ":" + port
-
-	// Get current working directory
-	if cwd, err = os.Getwd(); err != nil {
-		log.Fatal(err) // Panic if something goes wrong
+	// Get current working directory if not set with command line argument
+	if len(cwd) == 0 {
+		if cwd, err = os.Getwd(); err != nil {
+			log.Fatal(err) // Panic if something goes wrong
+		}
 	}
 
 	// Log a fatal error if a HTTP server fails
 	// Server listens to %port% and servers the contents of the current working directory
 	// If index.html (.htm not OK) is present, serve that on root request instead of directory listing
-	log.Fatal(http.ListenAndServe(port, logger(http.FileServer(http.Dir(cwd)))))
+	log.Fatal(http.ListenAndServe(":" + port, logger(http.FileServer(http.Dir(cwd)))))
 }
