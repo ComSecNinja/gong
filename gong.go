ec04c802854bb605295595f73c4b43bdd7e1d2a8
Timo Virkkunen <timo@comsec.ninja>
2015-10-20 04:17:54 +0300

    Update gong.go

--- gong.go
+++ gong.go
@@ -7,16 +7,15 @@ import (
 	"os"
 )
 
-// Declare all variables here
 var (
 	port   string
-	cwd    string
+	dir    string
 	err    error
 	lop    string
 	output *log.Logger
 )
 
-// Log the request & return the handler
+// Log the request & return the handler.
 func logger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		output.Println(r.RemoteAddr, r.Method, r.URL)
@@ -24,45 +23,54 @@ func logger(h http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	// Different port can be assigned with command line variable -port
+func parseFlags() {
 	flag.StringVar(&port, "port", "8080", "Assign a port for the server to listen to. Defaults to 8080.")
-	flag.StringVar(&cwd, "root", "", "Set a root directory for server. Defaults to current working directory.")
+	flag.StringVar(&dir, "root", "", "Set a root directory for server. Defaults to current working directory.")
 	flag.StringVar(&lop, "log", "", "Optional log output file.")
 	flag.Parse()
+}
 
-	// Get current working directory if not set with command line argument
-	if len(cwd) == 0 {
-		if cwd, err = os.Getwd(); err != nil {
-			log.Fatal(err) // Exit if something goes wrong
-		}
-	}
-
+func setLogFile(path string) {
 	// If log output file was set, open it and set it as the output.
-	if lop != "" {
+	if path != "" {
 		// Open the file write only. Use append and create it if nonexistant.
-		f, err := os.OpenFile(lop, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
 		if err != nil {
-			log.Fatal(err) // Something went wrong opening the log file.
+			log.Fatal(err) // Something went wrong while opening the log file.
 		}
-		defer f.Close() // Close the file when main exits.
+		defer f.Close() // Close the log file when main() exits.
 
 		output = log.New(f, "", log.Ldate | log.Ltime) // Set logger.
 		output.Println("Server started on port", port) // Log the start.
-		output.Println("Document root:", cwd) // Log the document root.
+		output.Println("Document root:", dir) // Log the document root.
 	} else {
-		// If -log was not used, use stdout.
+		// If a log file was not set, use stdout.
 		output = log.New(os.Stdout, "", log.Ldate | log.Ltime)
 	}
+}
+
+func init() {
+	parseFlags()
+
+	// Get current working directory if not set with command line argument.
+	if dir == "" {
+		if dir, err = os.Getwd(); err != nil {
+			log.Fatal(err) // Exit if something goes wrong.
+		}
+	}
+
+	setLogFile(lop)
 
 	// Print the assigned port on screen.
 	log.Println("Listening on port", port)
 
 	// Print the document root path on screen.
-	log.Println("Document root:", cwd)
+	log.Println("Document root:", dir)
+}
 
-	// Log a fatal error if a HTTP server fails
-	// Server listens to %port% and servers the contents of the current working directory
-	// If index.html (.htm not OK) is present, serve that on root request instead of directory listing
-	output.Fatal(http.ListenAndServe(":" + port, logger(http.FileServer(http.Dir(cwd)))))
+func main() {
+	// Log a fatal error if the HTTP server fails.
+	// Server listens on %port% and serves the contents of %dir%.
+	// If index.html (.htm not OK) is present, serve that on root request instead of directory listing.
+	output.Fatal(http.ListenAndServe(":" + port, logger(http.FileServer(http.Dir(dir)))))
 }
